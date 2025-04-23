c3d12b64d3353f061fbe2a62aab5f0c59b7e12af
aayman01 <ayman.alfi098@gmail.com>
2025-04-23 16:00:49 +0600

    nav bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar"
 
 
 function App() {
 
   return (
     <>
-      <h1 className="text-4xl text-red-500 font-bold">Check</h1>
+      <Navbar/>
     </>
   )
 }
